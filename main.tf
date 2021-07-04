b0cf623aa5856f88e453f1879c6b89e671bbdca6
Orfiaj Karim <gazi.karim@newscred.com>
2021-07-04 18:55:32 +0600

    Add all files

---
+++ main.tf
@@ -0,0 +1,86 @@
+provider "aws" {}
+
+resource "aws_vpc" "nc-vpc-1" {
+    cidr_block = var.cidr_block[0].cidr_block
+    tags = {
+        Name = var.cidr_block[0].name
+    }
+}
+
+data "aws_vpc" "selected" {
+    id = aws_vpc.nc-vpc-1.id
+}
+
+resource "aws_subnet" "nc-vpc-subnet-1" {
+    vpc_id = data.aws_vpc.selected.id
+    cidr_block = var.cidr_block[1].cidr_block
+    tags = {
+        Name: "${var.cidr_block[1].name}-${var.environment}"
+    }
+}
+resource "aws_default_route_table" "nc-dev-rtb" {
+  default_route_table_id = aws_vpc.nc-vpc-1.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nc-dev-igw.id
+  }
+
+  tags = {
+    Name = "${var.environment}-rtb"
+  }
+}
+
+
+resource "aws_internet_gateway" "nc-dev-igw" {
+  vpc_id = aws_vpc.nc-vpc-1.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+
+resource "aws_default_security_group" "nc-dev-default-sg" {
+    vpc_id = aws_vpc.nc-vpc-1.id
+
+    ingress {
+        description = "SSH Permission"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    egress {
+        description = "Allow traffic to reach outside"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.environment}-default-sg"
+    }
+
+}
+
+
+data "aws_ami" "latest-amazon-ami" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+    filter {
+      name = "root-device-type"
+      values = ["ebs"]
+    }
+}
+
+output "ami-info" {
+  value = data.aws_ami.latest-amazon-ami.id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+    description = "cidr block for vpc and subnets"
+}
+
+variable "environment" {
+    description = "Environment of the release"
+}
+
+variable "my_ip" {}
